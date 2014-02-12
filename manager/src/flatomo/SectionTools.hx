6d57e7095db890e1c5b26916efe6807e909e7e75
Tomosuke.St <tmskst@gmail.com>
2014-02-12 12:54:10 +0900

    toControlCodeを除去
    SectionKind.Goto の実装のために制御コードへの変換の仕方を修正

--- manager/src/flatomo/SectionTools.hx
+++ manager/src/flatomo/SectionTools.hx
@@ -11,31 +11,21 @@ class SectionTools {
 		var codes:Map<Int, ControlCode> = new Map<Int, ControlCode>();
 		
 		for (section in sections) {
-			var code = toControlCode(section);
-			if (code != null) {
-				codes.set(code.frame, code.code);
-			}
-		}
-		return codes;
-	}
-	
-	// 制御コードなしをnullで表現するのはどうかと思う
-	public static function toControlCode(section:Section): { frame:Int, code:ControlCode } {
-		switch (section.kind) {
+			switch (section.kind) {
 			case SectionKind.Loop : 
-				return { frame: section.end, code: ControlCode.Goto(section.begin) };
+				codes.set(section.end, ControlCode.Goto(section.begin));
 			case SectionKind.Once : 
-				return { frame: section.end, code: ControlCode.Stop };
+				codes.set(section.end, ControlCode.Stop);
 			case SectionKind.Pass, SectionKind.Default : 
 				// 追加するコードはない
-				return null;
 			case SectionKind.Standstill : 
-				return { frame: section.begin, code: ControlCode.Stop };
+				codes.set(section.begin, ControlCode.Stop);
 			case SectionKind.Goto(goto) : 
 				// TODO : SectionKind.Goto の仕様変更に追いついていません。
 				/* codes.set(section.end, ControlCode.Goto(goto)); */
-				return null;
+			}
 		}
+		return codes;
 	}
 	
 }
--- manager/test/flatomo/SectionToolsTest.hx
+++ manager/test/flatomo/SectionToolsTest.hx
@@ -6,55 +6,62 @@ import haxe.Json;
 import haxe.PosInfos;
 import massive.munit.Assert;
 
-private typedef Code = { frame:Int, code:ControlCode };
-
 class SectionToolsTest {
 
 	public function new() { }
 	
-	@Test("SectionKind.Loop: セクション内の最初のフレームにGoto(begin)を追加する")
-	public function toControlCodeTest_Loop():Void {
-		var expected = { frame: 10, code: ControlCode.Goto(1) };
-		var actual = SectionTools.toControlCode({ name: "anonymous", kind: SectionKind.Loop, begin: 1, end: 10 });
-		equals(expected, actual);
+	@Test("SectionKind.Loop: セクション内の最終フレームにGoto(begin)を追加する")
+	public function toControlCodes_Loop():Void {
+		var expected = ControlCode.Goto(1);
+		var codes = SectionTools.toControlCodes([
+			{ name: "anonymous", kind: SectionKind.Loop, begin: 1, end: 10 }
+		]);
+		var actual = codes.get(10);
+		Assert.areEqual(expected, actual);
 	}
 	
 	@Test("SectionKind.Once: セクション内の最終フレームにStopを追加する")
-	public function toControlCodeTest_Once():Void {
-		var expected = { frame: 10, code: ControlCode.Stop };
-		var actual = SectionTools.toControlCode({ name: "anonymous", kind: SectionKind.Once, begin: 1, end: 10 });
-		equals(expected, actual);
+	public function toControlCodes_Once():Void {
+		var expected = ControlCode.Stop;
+		var codes = SectionTools.toControlCodes([
+			{ name: "anonymous", kind: SectionKind.Once, begin: 1, end: 10 }
+		]);
+		var actual = codes.get(10);
+		Assert.areEqual(expected, actual);
 	}
 	
 	@Test("SectionKind.Standstill: セクション内の最初のフレームにStopを追加する")
 	public function toControlCodeTest_Standstill():Void {
-		var expected = { frame: 1, code: ControlCode.Stop };
-		var actual = SectionTools.toControlCode({ name: "anonymous", kind: SectionKind.Standstill, begin: 1, end: 10 });
-		equals(expected, actual);
+		var expected = ControlCode.Stop;
+		var codes = SectionTools.toControlCodes([
+			{ name: "anonymous", kind: SectionKind.Standstill, begin: 1, end: 10 }
+		]);
+		var actual = codes.get(1);
+		Assert.areEqual(expected, actual);
 	}
 	
 	@Test("SectionKind.Pass: セクション内にオペコードは存在しない")
 	public function toControlCodeTest_Pass():Void {
-		var actual = SectionTools.toControlCode({ name: "anonymous", kind: SectionKind.Pass, begin: 1, end: 10 });
-		Assert.isNull(actual);
+		var codes = SectionTools.toControlCodes([
+			{ name: "anonymous", kind: SectionKind.Pass, begin: 1, end: 10 }
+		]);
+		Assert.isFalse(codes.exists(1));
+		Assert.isFalse(codes.exists(10));
 	}
 	
 	@Test("SectionKind.Default: SectionKind.Passと同等")
 	public function toControlCodeTest_Default():Void {
-		var expected = null;
-		var actual = SectionTools.toControlCode({ name: "anonymous", kind: SectionKind.Default, begin: 1, end: 10 });
-		Assert.areEqual(expected, actual);
+		var codes = SectionTools.toControlCodes([
+			{ name: "anonymous", kind: SectionKind.Default, begin: 1, end: 10 }
+		]);
+		Assert.isFalse(codes.exists(1));
+		Assert.isFalse(codes.exists(10));
 	}
 	
 	@Ignore("未実装")
 	@Test("SectionKind.Goto: ...")
 	public function toControlCodeTest_Goto():Void {
 		// ...
-	}
-	
-	private static inline function equals(expected:Code, actual:Code):Void {
-		Assert.areEqual(expected.frame, actual.frame);
-		Assert.areEqual(expected.code, actual.code);
 	}
 	
 	@Test("すべてのセクションが例外なく変換できる")
