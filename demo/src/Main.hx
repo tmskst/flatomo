a0c81c94f70281361988526057bf0ead4804f705
Tomosuke.St <tmskst@gmail.com>
2014-02-19 22:35:18 +0900

    デモを更新

--- demo/src/Main.hx
+++ demo/src/Main.hx
@@ -1,9 +1,6 @@
 package ;
-import flash.display.Loader;
-import flash.events.Event;
-import flash.net.URLRequest;
-import flash.system.ApplicationDomain;
-import flash.system.LoaderContext;
+import flatomo.Animation;
+import flatomo.Container;
 import flatomo.Flatomo;
 import starling.display.DisplayObject;
 import starling.display.Sprite;
@@ -12,19 +9,8 @@ class Main extends Sprite {
 	
 	public function new() {
 		super();
-		var ASSET_PATH = "library.swf";
-		
-		var loader:Loader = new Loader();
-		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, deploy);
-		loader.load(
-			new URLRequest(ASSET_PATH),
-			new LoaderContext(false, ApplicationDomain.currentDomain)
-		);
-	}
-	
-	private function deploy(event:Event):Void {
-		Flatomo.start(Type.createInstance(Type.resolveClass("Config"), []));
-		var object:DisplayObject = Flatomo.create(Type.createInstance(Type.resolveClass("TestMovie"), []));
+		Flatomo.start(new Config());
+		var object:DisplayObject = cast(Flatomo.create(new TestMovie()), DisplayObject);
 		this.addChild(object);
 	}
 	
