5c6b5e8c26d66dd5e0e61589a6953a6e63a31a2c
Tomosuke.St <tmskst@gmail.com>
2014-03-06 23:33:27 +0900

    メタデータを外部書き出し

--- extension/script/src/flatomo/extension/Publisher.hx
+++ extension/script/src/flatomo/extension/Publisher.hx
@@ -4,6 +4,7 @@ import flatomo.FlatomoTools;
 import haxe.Serializer;
 import jsfl.Document;
 import jsfl.EventType;
+import jsfl.FLfile;
 import jsfl.Lib.fl;
 
 using flatomo.extension.DocumentTools;
@@ -12,16 +13,22 @@ class Publisher {
 	
 	private static var id:Int;
 	
-	@:access(flatomo.FlatomoTools)
 	public static function main() {
 		var document:Document = fl.getDocumentDOM();
 		if (!document.isFlatomo()) { return; }
-		
-		var obj = FlatomoTools.createLibrary(document.library);
-		fl.trace(Serializer.run(obj));
+		writeLibrary();
 		
 		id = fl.addEventListener(EventType.POST_PUBLISH, postPublish);
 		document.publish();
+	}
+	
+	@:access(flatomo.FlatomoTools)
+	private static function writeLibrary():Void {
+		var document:Document = fl.getDocumentDOM();
+		var library = FlatomoTools.createLibrary(document.library);
+		var swfPath:String = document.getSWFPathFromProfile();
+		var fileUri = swfPath.substring(0, swfPath.lastIndexOf(".")) + "." + "flatomo";
+		FLfile.write(fileUri, Serializer.run(library));
 	}
 	
 	@:access(flatomo.FlatomoTools)
@@ -29,4 +36,5 @@ class Publisher {
 		FlatomoTools.clean(fl.getDocumentDOM().library);
 		fl.removeEventListener(EventType.POST_PUBLISH, id);
 	}
+	
 }
