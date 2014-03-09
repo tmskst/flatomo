d770e63b1afce40b6e88e34570f4e47ff525a162
Tomosuke.St <tmskst@gmail.com>
2014-03-09 22:53:24 +0900

    update Creator

--- manager/src/flatomo/Creator.hx
+++ manager/src/flatomo/Creator.hx
@@ -2,6 +2,7 @@ package flatomo;
 
 import flash.display.BitmapData;
 import flash.display.DisplayObject;
+import flash.display.DisplayObjectContainer;
 import flash.display.MovieClip;
 import flash.geom.Rectangle;
 
