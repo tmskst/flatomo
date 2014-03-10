915d7deb7626938e7116ebdf285e201e01cf732e
Tomosuke.St <tmskst@gmail.com>
2014-03-11 02:41:07 +0900

    add Debugger
    デバッグ用にテクスチャアトラスを出力する機能を追加

---
+++ manager/src/flatomo/Debugger.hx
@@ -0,0 +1,40 @@
+package flatomo;
+
+class Debugger {
+	
+	public static function export(atlases:Array<{ image:flash.display.BitmapData, layout:flash.xml.XML }>):Void {
+		var entries:List<format.zip.Data.Entry> = new List();
+		for (index in 0...atlases.length) {
+			var atlas = atlases[index];
+			entries.add(ofImage(atlas.image, 'atlas${index}.png'));
+			entries.add(ofXml(atlas.layout, 'atlas${index}.xml'));
+		}
+		var output = new haxe.io.BytesOutput();
+		new haxe.zip.Writer(output).write(entries);
+		new flash.net.FileReference().save(output.getBytes().getData(), "atlas.zip");
+	}
+	
+	private static function ofXml(xml:flash.xml.XML, fileName:String):haxe.zip.Entry {
+		var bytes = haxe.io.Bytes.ofString(xml.toXMLString());
+		return toEntry(bytes, fileName);
+	}
+	
+	private static function ofImage(image:flash.display.BitmapData, fileName:String):haxe.zip.Entry {
+		var bytes = haxe.io.Bytes.ofData(image.encode(image.rect, new flash.display.PNGEncoderOptions()));
+		return toEntry(bytes, fileName);
+	}
+	
+	private static function toEntry(bytes:haxe.io.Bytes, fileName:String):haxe.zip.Entry {
+		return {
+			fileName : fileName,
+			fileSize : bytes.length,
+			fileTime : Date.now(),
+			compressed : false,
+			dataSize : 0,
+			data : bytes,
+			crc32 : haxe.crypto.Crc32.make(bytes),
+			extraFields : new List()
+		};
+	}
+	
+}
