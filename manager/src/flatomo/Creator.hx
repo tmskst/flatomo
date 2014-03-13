7feabea95d538a569e06f87d0e3c213c8b26fcee
Tomosuke.St <tmskst@gmail.com>
2014-03-14 02:50:53 +0900

    update Creator

--- manager/src/flatomo/Creator.hx
+++ manager/src/flatomo/Creator.hx
@@ -96,11 +96,11 @@ class Creator {
 					case DisplayObjectType.Image : '${libraryPath}#${child.name}';
 				}
 				if (!children.exists(function (x) {
-					return (x.key == childKey && x.instanceName == child.name);
+					return (x.instanceName == child.name);
 				})) {
 					children.push({ key: childKey, instanceName: child.name });
+					translate(child, childKey);
 				}
-				translate(child, childKey);
 				
 				layouts.push({
 					instanceName: child.name,
