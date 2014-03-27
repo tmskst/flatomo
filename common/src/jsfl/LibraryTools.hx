d5c28dba64836c08d915c0d909668337061afbdc
Tomosuke.St <tmskst@gmail.com>
2014-03-28 02:06:38 +0900

    ライブラリにフォントアイテムがあったときに書き出せなかったので修正

--- common/src/jsfl/LibraryTools.hx
+++ common/src/jsfl/LibraryTools.hx
@@ -7,8 +7,8 @@ class LibraryTools {
 	 */
 	public static function scan_allSymbolItem(library:Library, func:SymbolItem -> Void):Void {
 		for (item in library.items) {
-			if (Std.is(item, SymbolItem)) {
-				func(cast(item, SymbolItem));
+			switch (item.itemType) {
+				case ItemType.GRAPHIC, ItemType.MOVIE_CLIP : func(cast item);
 			}
 		}
 	}
