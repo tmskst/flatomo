e3d41111d53474262d5ec30df0e32bae815b1d34
Tomosuke.St <tmskst@gmail.com>
2014-05-06 01:01:56 +0900

    スプライトシート書き出しで利用しないPostureのコンストラクタをflashに限定

--- common/src/flatomo/Posture.hx
+++ common/src/flatomo/Posture.hx
@@ -1,15 +1,19 @@
 package flatomo;
 
+#if flash
 import flash.text.TextFormat;
+#end
 import haxe.ds.Vector.Vector;
 
 /** 表示オブジェクトを再構築するために必要な情報 */
 enum Posture {
 	Animation(sections:Array<Section>, pivotX:Float, pivotY:Float);
+	#if flash
 	/**
 	 * @param layouts コンテナの直接の子の配置情報
 	 */
 	Container(children:Map<InstanceName, { path:String, layouts:Vector<Layout> }>, sections:Array<Section>);
 	Image(pivotX:Float, pivotY:Float);
 	TextField(width:Int, height:Int, text:String, textFormat:TextFormat);
+	#end
 }
