22d8dc6dd9290935fe33bdd2647d88ee256c928b
Tomosuke.St <tmskst@gmail.com>
2014-07-14 18:02:03 +0900

    update DocumentConfig.hx
    ドキュメントがFlatomo向けかどうか拡張パネルで指定できるように変更

--- extension/script/src/flatomo/DocumentConfig.hx
+++ extension/script/src/flatomo/DocumentConfig.hx
@@ -1,10 +1,37 @@
 package flatomo;
 
 import haxe.Resource;
+import haxe.Template;
+import jsfl.Document;
+import jsfl.Lib;
 import jsfl.Lib.fl;
 
+using flatomo.extension.DocumentTools;
+
 class DocumentConfig {
+	
+	@:access(flatomo.extension.DocumentTools)
 	public static function main() {
-		fl.xmlPanelFromString(Resource.getString("DocumentConfig"));
+		var document:Document = fl.getDocumentDOM();
+		if (document == null) {
+			return Lib.alert("有効なドキュメントを開いてください");
+		}
+		
+		var documentConfigTemplate = new Template(Resource.getString("DocumentConfig"));
+		var result:Dynamic = fl.xmlPanelFromString(documentConfigTemplate.execute(
+			{ // context
+				isFlatomoDocument: document.isFlatomo(),
+			}
+		));
+		
+		// ダイアログがキャンセルされたら保存せずに終了
+		if (result.dismiss == "cancel") { return; }
+		
+		// ドキュメントの情報を更新
+		var latestFlatomoDocument:Bool = result.isFlatomoDocument == "true";
+		switch (latestFlatomoDocument) {
+			case true	: document.enableFlatomo();
+			case false	: document.disableFlatomo();
+		}
 	}
 }
