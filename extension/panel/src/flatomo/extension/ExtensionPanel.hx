b15f74e46dead22bb539a6c8630099ea5a46b741
Tomosuke.St <tmskst@gmail.com>
2014-07-14 17:22:46 +0900

    ExtensionPanel.hxをリファクタリング

--- extension/panel/src/flatomo/extension/ExtensionPanel.hx
+++ extension/panel/src/flatomo/extension/ExtensionPanel.hx
@@ -1,10 +1,12 @@
 package flatomo.extension;
 
+import com.bit101.components.Label;
 import com.bit101.components.PushButton;
 import com.bit101.components.Style;
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.Lib;
+import haxe.macro.Compiler;
 
 class ExtensionPanel extends Sprite {
 	
@@ -16,42 +18,49 @@ class ExtensionPanel extends Sprite {
 	public function new() {
 		super();
 		
-		openSymbolItemConfigButton = new PushButton(this, 10, 20, "OPEN SYMBOL ITEM CONFIG", openSymbolItemConfig);
-		openSymbolItemConfigButton.height = 20;
+		flatomoVersionLabel = new Label(this, 0, 65, Compiler.getDefine("flatomoVersion"));
 		
-		publishButton = new PushButton(this, 10, 40, "PUBLISH", publish);
-		publishButton.height = 20;
+		openFlatomoItemConfigDialogButton = new PushButton(this, 10, 5, "OPEN SYMBOL ITEM CONFIG", openSymbolItemConfig);
+		openFlatomoItemConfigDialogButton.height = 20;
 		
-		openDocumentConfigButton = new PushButton(this, 10, 60, "OPEN DOCUMENT CONFIG", openDocumentConfig);
-		openDocumentConfigButton.height = 20;
+		openPublishDialogButton = new PushButton(this, 10, 25, "PUBLISH", publish);
+		openPublishDialogButton.height = 20;
+		
+		openDocumentConfigDialogButton = new PushButton(this, 10, 45, "OPEN DOCUMENT CONFIG", openDocumentConfig);
+		openDocumentConfigDialogButton.height = 20;
 		
 		resize(null);
 		Lib.current.stage.addEventListener(Event.RESIZE, resize);
 	}
 	
-	private var openSymbolItemConfigButton:PushButton;
-	private var publishButton:PushButton;
-	private var openDocumentConfigButton:PushButton;
+	private var flatomoVersionLabel:Label;
+	private var openFlatomoItemConfigDialogButton:PushButton;
+	private var openPublishDialogButton:PushButton;
+	private var openDocumentConfigDialogButton:PushButton;
 	
 	private function resize(event:Event = null):Void {
-		var stageWidth = Lib.current.stage.stageWidth;
-		var stageHeight = Lib.current.stage.stageHeight;
+		var buttonWidth = Lib.current.stage.stageWidth - 20;
 		
-		openSymbolItemConfigButton.width = stageWidth - 20;
-		publishButton.width = stageWidth - 20;
-		openDocumentConfigButton.width = stageWidth - 20;
+		flatomoVersionLabel.x = buttonWidth - flatomoVersionLabel.width + 10;
+		openFlatomoItemConfigDialogButton.width = buttonWidth;
+		openPublishDialogButton.width = buttonWidth;
+		openDocumentConfigDialogButton.width = buttonWidth;
 	}
 	
 	private function openSymbolItemConfig(event:Event):Void {
-		untyped __global__["adobe.utils.MMExecute"]('fl.runScript(fl.configURI + "WindowSWF/FlatomoItemConfig.jsfl")');
+		execute("WindowSWF/FlatomoItemConfig.jsfl");
 	}
 	
 	private function publish(event:Event):Void {
-		untyped __global__["adobe.utils.MMExecute"]('fl.runScript(fl.configURI + "WindowSWF/PublishDialog.jsfl")');
+		execute("WindowSWF/PublishDialog.jsfl");
 	}
 	
 	private function openDocumentConfig(event:Event):Void {
-		untyped __global__["adobe.utils.MMExecute"]('fl.runScript(fl.configURI + "WindowSWF/DocumentConfig.jsfl")');
+		execute("WindowSWF/DocumentConfig.jsfl");
+	}
+	
+	private function execute(fileName:String):Void {
+		untyped __global__["adobe.utils.MMExecute"]('fl.runScript(fl.configURI + "$fileName")');
 	}
 	
 }
