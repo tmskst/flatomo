864185d7af6930c6046a810e4badf61e0809b489
Tomosuke.St <tmskst@gmail.com>
2014-07-14 18:33:35 +0900

    update PublishDialog.hx
    ダイアログからアセットをパブリッシュできるように変更

--- extension/script/src/flatomo/PublishDialog.hx
+++ extension/script/src/flatomo/PublishDialog.hx
@@ -1,10 +1,40 @@
 package flatomo;
 
 import haxe.Resource;
+import haxe.Template;
+import jsfl.Document;
+import jsfl.Lib;
 import jsfl.Lib.fl;
 
+using flatomo.extension.DocumentTools;
+
 class PublishDialog {
+	
 	public static function main() {
-		fl.xmlPanelFromString(Resource.getString("PublishDialog"));
+		var document:Document = fl.getDocumentDOM();
+		if (document == null) {
+			return Lib.alert("有効なドキュメントを開いてください");
+		}
+		if (!document.isFlatomo()) {
+			return Lib.alert("Flatomoが有効でないドキュメントです");
+		}
+		
+		var publishDialogTemplate = new Template(Resource.getString("PublishDialog"));
+		var result:Dynamic = fl.xmlPanelFromString(publishDialogTemplate.execute(
+			{ // context
+				dynamicPublish: false,
+				staticPublish : false,
+			}
+		));
+		
+		// ダイアログがキャンセルされたら保存せずに終了
+		if (result.dismiss == "cancel") { return; }
+		
+		var staticPublish:Bool	= result.staticPublish == "true";
+		if (staticPublish) { Exporter.run(); }
+		
+		var dynamicPublish:Bool	= result.dynamicPublish == "true";
+		if (dynamicPublish) { Publisher.run(); }
+		
 	}
 }
