10220ac132c239f54968a3326799c677c103e5ba
Tomosuke.St <tmskst@gmail.com>
2014-07-16 02:44:39 +0900

    BitmapItemをテクスチャとして扱えるようにTextureAtlasExporterを修正

--- extension/script/src/flatomo/extension/exporter/TextureAtlasExporter.hx
+++ extension/script/src/flatomo/extension/exporter/TextureAtlasExporter.hx
@@ -1,6 +1,7 @@
 package flatomo.extension.exporter;
+import jsfl.Item;
+import jsfl.ItemType;
 import jsfl.SpriteSheetExporter;
-import jsfl.SymbolItem;
 
 // FIXME : 1枚に収まりきらないスプライトシートの対応
 class TextureAtlasExporter {
@@ -9,7 +10,7 @@ class TextureAtlasExporter {
 	 * スプライトシートの出力。
 	 * SWFプロファイルに設定されたディレクトリに `flaファイル名 + (.png, .xml)` が出力される。
 	 */
-	public static function export(symbolItems:Array<SymbolItem>, outputPath:String):Void {
+	public static function export(items:Array<Item>, outputPath:String):Void {
 		var exporter = new SpriteSheetExporter();
 		{ // initialize SpriteSheetExporter
 			exporter.algorithm = SpriteSheetExporterAlgorithm.MAX_RECTS;
@@ -20,8 +21,13 @@ class TextureAtlasExporter {
 		}
 		
 		// シンボルアイテムをエクスポータに追加
-		for (symbolItem in symbolItems) {
-			exporter.addSymbol(symbolItem);
+		for (item in items) {
+			switch (item.itemType) {
+				case ItemType.BITMAP :
+					exporter.addBitmap(cast item);
+				case ItemType.MOVIE_CLIP, ItemType.GRAPHIC :
+					exporter.addSymbol(cast item);
+			}
 		}
 		
 		// スプライトシートを出力
