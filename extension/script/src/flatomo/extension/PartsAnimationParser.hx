2d3915b6deca46822f0a4aa02cb9942b2d77b914
Tomosuke.St <tmskst@gmail.com>
2014-07-21 00:24:50 +0900

    PartsAnimationParserをリファクタリング

--- extension/script/src/flatomo/extension/PartsAnimationParser.hx
+++ extension/script/src/flatomo/extension/PartsAnimationParser.hx
@@ -1,22 +1,20 @@
 package flatomo.extension;
 
-import flatomo.FlatomoItem;
 import jsfl.ElementType;
 import jsfl.Frame;
 import jsfl.Instance;
 import jsfl.InstanceType;
-import jsfl.Layer;
 import jsfl.LayerType;
 import jsfl.Matrix;
 import jsfl.MatrixTools;
 import jsfl.SymbolInstance;
 import jsfl.SymbolItem;
 import jsfl.SymbolType;
-import jsfl.Timeline;
+import jsfl.Tween;
 import jsfl.TweenType;
-import jsfl.Lib.fl;
 
 using Lambda;
+using jsfl.MatrixTools;
 using flatomo.extension.util.ItemTools;
 
 class PartsAnimationParser {
@@ -55,72 +53,70 @@ class PartsAnimationParser {
 		analyze(rootSymbolItem, frameIndex, new Array<Matrix>());
 	}
 	
-	private function analyze(symbolItem:SymbolItem, frameIndex:Int, stack:Array<Matrix>):Void {
-		var timeline:Timeline = symbolItem.timeline;
-		//trace(symbolItem.name + ", " + frameIndex);
+	private function analyze(symbolItem:SymbolItem, frameIndex:Int, matrixes:Array<Matrix>):Void {
+		//{ 走査するフレーム（フレームが存在する通常レイヤー）
+		var frames:Array<Frame> = symbolItem.timeline.layers
+			.filter(function (layer) { return layer.layerType.equals(LayerType.NORMAL); })
+			.map   (function (layer) { return layer.frames[frameIndex]; } )
+			.filter(function (frame) { return frame != null; } );
+		//} 
+		frames.reverse();
 		
-		for (layerIndex in 0...timeline.layerCount) {
-			var layer:Layer = timeline.layers[(timeline.layerCount - 1) - layerIndex];
-			if (layer.layerType.equals(LayerType.GUIDE) || layer.layerType.equals(LayerType.GUIDED)) { continue; }
+		for (frame in frames) {
+			var geometricTransform:Matrix = switch (frame.tweenType) {
+				case TweenType.NONE, TweenType.SHAPE :
+					MatrixTools.createIdentityMatrix();
+				case TweenType.MOTION, TweenType.MOTION_OBJECT :
+					var tween:Tween = frame.tweenObj;
+					tween.getGeometricTransform(frameIndex - tween.startFrame);
+			};
 			
-			var frame:Frame = layer.frames[frameIndex];
-			if (frame == null) { continue; }
+			// 走査するインスタンス（フレームに配置されたエレメントのうちインスタンスだけ）
+			var instances:Array<Instance> = untyped frame.elements
+				.filter(function (element) { return element.elementType.equals(ElementType.INSTANCE); } );
 			
-			// Tween.geometricTransform
-			var geometricTransform:Matrix = if (frame.tweenType == TweenType.NONE) {
-				MatrixTools.createIdentityMatrix();
-			} else {
-				frame.tweenObj.getGeometricTransform(frameIndex - frame.tweenObj.startFrame);
-			}
-			
-			for (element in frame.elements) {
-				if (!element.elementType.equals(ElementType.INSTANCE)) { continue; }
-				var instance:Instance = cast element;
-				stack.push(MatrixTools.concatMatrix(instance.matrix, geometricTransform));
+			for (instance in instances) {
+				matrixes.push(MatrixTools.concatMatrix(instance.matrix, geometricTransform));
 				
-				var isPrimitiveInstance:Bool = switch (instance.instanceType) {
-					case InstanceType.BITMAP : true;
-					case InstanceType.SYMBOL : cast(instance.libraryItem, SymbolItem).getFlatomoItem().primitiveItem;
-					case _ : false;
-				};
+				// パーツアニメーションの基本単位（プリミティブインスタンス）
+				// 1. インスタンスがビットマップならば強制的にパーツアニメーションの基本単位とみなす
+				// 2. 拡張アイテムについて`プリミティブ属性`が有効ならパーツアニメーションの基本単位とする
+				var primitive:Bool = 
+					instance.instanceType == InstanceType.BITMAP ||
+					instance.instanceType == InstanceType.SYMBOL && ItemTools.getFlatomoItem(untyped instance.libraryItem).primitiveItem;
 				
-				// fold
-				if (isPrimitiveInstance) {
-					var result:Matrix = MatrixTools.createIdentityMatrix();
-					for (matrix in stack) {
-						result = MatrixTools.concatMatrix(matrix, result);
-					}
-					//trace(instance.libraryItem.name + ", " + depth);
-					parts.push( { name: instance.libraryItem.name, matrix: result, id: -1, depth: depth++ } );
-					stack.pop();
-					continue;
+				// インスタンスがパーツアニメーションの基本単位
+				if (primitive) {
+					var result:Matrix = matrixes
+						.fold(function (matrix1, matrix2) { return matrix1.concatMatrix(matrix2); }, MatrixTools.createIdentityMatrix());
+					
+					parts.push({ name: instance.libraryItem.name, matrix: result, id: -1, depth: depth++ });
+					matrixes.pop();
 				}
-				if (instance.instanceType == InstanceType.SYMBOL) {
+				// パーツアニメーションの基本単位ではないシンボルインスタンス（インスタンスが子を持っている）
+				else if (instance.instanceType == InstanceType.SYMBOL) {
 					var symbolInstance:SymbolInstance = cast instance;
+					if (symbolInstance.symbolType.equals(SymbolType.BUTTON)) { continue; }
+					
 					var firstFrame:Int = symbolInstance.firstFrame;
 					var frameCount:Int = cast(symbolInstance.libraryItem, SymbolItem).timeline.frameCount;
 					
-					var symbolFrame:Int = switch (symbolInstance.symbolType) {
+					var symbolFrameIndex:Int = switch (symbolInstance.symbolType) {
 						case SymbolType.MOVIE_CLIP : 0;
 						case SymbolType.GRAPHIC :
 							switch (symbolInstance.loop) {
-								case LoopType.LOOP :
-									(frameIndex + firstFrame - frame.startFrame) % frameCount;
-								case LoopType.PLAY_ONCE :
-									Math.ceil(Math.min(frameIndex + firstFrame - frame.startFrame, frameCount - 1));
-								case LoopType.SINGLE_FRAME :
-									firstFrame;
+								case LoopType.LOOP			: (frameIndex + firstFrame - frame.startFrame) % frameCount;
+								case LoopType.PLAY_ONCE 	: Math.ceil(Math.min(frameIndex + firstFrame - frame.startFrame, frameCount - 1));
+								case LoopType.SINGLE_FRAME	: firstFrame;
 							};
 						case SymbolType.BUTTON :
 							throw 'Ignored : Button Symbol';
 					}
-					//trace("SYMBOL FRAME : " + symbolFrame);
-					analyze(cast symbolInstance.libraryItem, symbolFrame, stack);
+					
+					analyze(cast symbolInstance.libraryItem, symbolFrameIndex, matrixes);
 				}
-				
 			}
 		}
-		stack.pop();
+		matrixes.pop();
 	}
-	
 }
