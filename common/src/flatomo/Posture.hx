7dd5a11d3c8cd9605da0baa85fc40b92066d7493
Tomosuke.St <tmskst@gmail.com>
2014-08-05 16:51:44 +0900

    extensionからPosture.Containerへ参照可能に

--- common/src/flatomo/Posture.hx
+++ common/src/flatomo/Posture.hx
@@ -7,11 +7,8 @@ import flash.text.TextFormat;
 /** 表示オブジェクトを再構築するために必要な情報 */
 enum Posture {
 	Animation(sections:Array<Section>);
-	#if flash
-	/**
-	 * @param layouts コンテナの直接の子の配置情報
-	 */
 	Container(children:Map<InstanceName, { path:String, layouts:Array<Layout> }>, sections:Array<Section>);
+	#if flash
 	Image;
 	TextField(width:Int, height:Int, text:String, textFormat:TextFormat);
 	#end
