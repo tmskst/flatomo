372b95fbd8c148aa8ce5781c11ec06ef5a7d2f22
Tomosuke.St <tmskst@gmail.com>
2014-08-05 17:34:15 +0900

    Posture.Imageをextensionから参照可能に
    パーツアニメーションの基本単位はFlatomoImageであるからextensionから生成する必要がある。

--- common/src/flatomo/Posture.hx
+++ common/src/flatomo/Posture.hx
@@ -8,8 +8,8 @@ import flash.text.TextFormat;
 enum Posture {
 	Animation(sections:Array<Section>);
 	Container(children:Map<InstanceName, { path:String, layouts:Array<Layout> }>, sections:Array<Section>);
-	#if flash
 	Image;
+	#if flash
 	TextField(width:Int, height:Int, text:String, textFormat:TextFormat);
 	#end
 }
