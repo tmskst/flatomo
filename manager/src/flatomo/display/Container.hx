f8b2063c7d571357ee9afa8fba16cfd7e69a0c13
Tomosuke.St <tmskst@gmail.com>
2014-08-05 11:14:16 +0900

    flatomo.display.Containerをリファクタリング

--- manager/src/flatomo/display/Container.hx
+++ manager/src/flatomo/display/Container.hx
@@ -11,13 +11,11 @@ import starling.display.DisplayObjectContainer;
 
 using flatomo.display.SectionTools;
 
-// TODO : 将来、コンテナの親（継承関係）は starling.display.Sprite に置き換わる可能性があります。
-
 /**
  * フレームとセクションで構成される表示オブジェクトコンテナ機能を提供する。
- * すべてのコンテナの子は、コンテナの生成と同時に追加（addChild）される。
- * また、コンテナ内のインスタンス名（Element#name）は、1つの表示オブジェクトに対応する。
- * この性質上、再生ヘッドの到達を待つことなく任意の子へのアクセスができる。
+ * すべてのコンテナの子は、生成と同時にコンテナに追加（addChild）される。
+ * 1つのインスタンス名（任意のコンテナの子の名前）は、1つの表示オブジェクトに対応し、
+ * この性質上、再生ヘッドの到達を待つことなく任意の子へアクセスすることができる。
  */
 class Container extends DisplayObjectContainer implements ILayoutAdjusted {
 	
@@ -37,7 +35,6 @@ class Container extends DisplayObjectContainer implements ILayoutAdjusted {
 		
 		// すべての表示オブジェクトは、再生ヘッドの位置に関係なく常にコンテナに追加されている。
 		for (object in displayObjects) {
-			// FIXME : ジャグラーに登録しないとコンテナは可視状態にならない
 			object.visible = false;
 			this.addChild(object);
 		}
