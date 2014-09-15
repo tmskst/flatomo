4c35f921eb88dbb3bfb773e1b3b03471f1e7455a
Tomosuke.St <tmskst@gmail.com>
2014-09-15 23:13:11 +0900

    LibraryTools.symbolItemsの戻り値の型をArray<SymbolItem>に変更

--- extension/script/src/flatomo/util/LibraryTools.hx
+++ extension/script/src/flatomo/util/LibraryTools.hx
@@ -2,12 +2,13 @@ package flatomo.util;
 
 import jsfl.Item;
 import jsfl.Library;
+import jsfl.SymbolItem;
 
 class LibraryTools {
 	
 	/** ライブラリのうちシンボルアイテムのみの列挙 */
-	public static function symbolItems(library:Library):Array<Item> {
-		return library.items.filter(function (item) { return item.itemType.equals(GRAPHIC) || item.itemType.equals(MOVIE_CLIP); });
+	public static function symbolItems(library:Library):Array<SymbolItem> {
+		return untyped library.items.filter(function (item) { return item.itemType.equals(GRAPHIC) || item.itemType.equals(MOVIE_CLIP); });
 	}
 	
 	/** ライブラリから指定したnamePathのアイテムを取得する */
