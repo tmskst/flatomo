7393374d444d73b4d768e7fb11f042b15a4526c1
Tomosuke.St <tmskst@gmail.com>
2014-09-19 14:24:29 +0900

    EmbedAssetをリファクタリングしつつgetXmlを実装

--- runtime/src/EmbedAsset.hx
+++ runtime/src/EmbedAsset.hx
@@ -1,5 +1,10 @@
 package ;
 
+#if flash
+import flash.xml.XML;
+import haxe.io.BytesData;
+#end
+
 @:allow(flatomo.macro.EmbedAssetUtil)
 class EmbedAsset {
 	
@@ -21,8 +26,24 @@ class EmbedAsset {
 		return EMBED_ASSET_PACKAGE.join('.') + '.' + className;
 	}
 	
+	#if flash
+	
+	
+	
+	/**
+	 * キーに対応するテクスチャを返します
+	 * ただしテクスチャの型は 'flash.display.BitmapData' または 'haxe.io.BytesData' です
+	 */
 	public static function getTexture(key:String):Dynamic {
 		return Type.createInstance(Type.resolveClass(getClassPath(getTextureClassName(key))), []);
 	}
+	
+	/** キーに対応するXMLを返します */
+	public static function getXml(key:String):XML {
+		return new XML(Type.createInstance(Type.resolveClass(getClassPath(getXmlClassName(key))), []));
+	}
+	
+	
+	#end
 	
 }
