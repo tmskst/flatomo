74e782d1f1ade4a08c934c26f056d44247d41bff
Tomosuke.St <tmskst@gmail.com>
2014-09-21 01:33:49 +0900

    [cleanup] EmbedAssetをリファクタリング

--- runtime/src/EmbedAsset.hx
+++ runtime/src/EmbedAsset.hx
@@ -1,4 +1,7 @@
 package ;
+import flatomo.ItemPath;
+import flatomo.Structure;
+import haxe.Unserializer;
 
 #if flash
 import flash.xml.XML;
@@ -10,46 +13,60 @@ import haxe.io.BytesData;
 #end
 class EmbedAsset {
 	
+	/* アセットから埋め込みアセットのクラス名を解決するために使われる */
 	@:resolver
-	public static var resolver:Map<Asset, String>;
+	private static var resolver:Map<Asset, String>;
 	
+	/* 埋め込みアセットのクラスの出力先パッケージ */
 	@:allow(flatomo.macro.EmbedAssetUtil)
 	private static var EMBED_ASSET_PACKAGE:Array<String> = ['flatomo', 'macro', 'embedAsset'];
 	
+	/* アセット名から埋め込みテクスチャのクラス名を取得する */
 	@:allow(flatomo.macro.EmbedAssetUtil)
-	private static function getTextureClassName(className:String):String {
-		return className + 'Texture';
+	private static inline function getTextureClassName(assetName:String):String {
+		return assetName + 'Texture';
 	}
 	
+	/* アセット名から埋め込みXMLのクラス名を取得する */
 	@:allow(flatomo.macro.EmbedAssetUtil)
-	private static function getXmlClassName(className:String):String {
-		return className + 'Xml';
+	private static inline function getXmlClassName(assetName:String):String {
+		return assetName + 'Xml';
 	}
 	
+	/* アセット名から埋め込みストラクチャのクラス名を取得する */
 	@:allow(flatomo.macro.EmbedAssetUtil)
-	private static function getPostureClassName(className:String):String {
-		return className + 'Posture';
+	private static inline function getPostureClassName(assetName:String):String {
+		return assetName + 'Posture';
 	}
 	
+	/* 埋め込みアセットのクラスの完全修飾名を取得する */
 	private static function getClassPath(className:String):String { 
 		return EMBED_ASSET_PACKAGE.join('.') + '.' + className;
 	}
 	
 	#if flash
 	
+	/* クラス名を元にインスタンスを生成するヘルパメソッド */
+	private static inline function create(className:String):Dynamic {
+		return Type.createInstance(Type.resolveClass(className), []);
+	}
+	
 	/**
 	 * キーに対応するテクスチャを返します
 	 * ただしテクスチャの型は 'flash.display.BitmapData' または 'haxe.io.BytesData' です
 	 */
 	public static function getTexture(key:Asset):Dynamic {
-		return Type.createInstance(Type.resolveClass(getClassPath(getTextureClassName(resolver.get(key)))), []);
+		return create(getClassPath(getTextureClassName(resolver.get(key))));
 	}
 	
 	/** キーに対応するXMLを返します */
 	public static function getXml(key:Asset):XML {
-		return new XML(Type.createInstance(Type.resolveClass(getClassPath(getXmlClassName(resolver.get(key)))), []));
+		return new XML(create(getClassPath(getXmlClassName(resolver.get(key)))));
 	}
 	
+	public static function getPosture(key:Asset):Map<ItemPath, Structure> {
+		return haxe.Unserializer.run(create(getClassPath(getPostureClassName(resolver.get(key)))));
+	}
 	
 	#end
 	
