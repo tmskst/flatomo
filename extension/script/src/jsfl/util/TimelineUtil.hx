235094aecf00477727e543f53ce91828bb826571
Tomosuke.St <tmskst@gmail.com>
2014-10-28 14:02:45 +0900

    タイムラインに存在するすべてのフレームの領域（bounds）の和を返す getUnionBoundsを実装

--- extension/script/src/jsfl/util/TimelineUtil.hx
+++ extension/script/src/jsfl/util/TimelineUtil.hx
@@ -1,5 +1,6 @@
 package jsfl.util;
 
+import jsfl.BoundingRectangle;
 import jsfl.ElementType;
 import jsfl.Instance;
 import jsfl.Layer;
@@ -24,6 +25,18 @@ class TimelineUtil {
 	/** タイムライン中の'NORMAL'レイヤーを取得する */
 	public static function normalLayers(timeline:Timeline):Array<Layer> {
 		return timeline.layers.filter(function (layer) return layer.layerType.equals(LayerType.NORMAL));
+	}
+	
+	public static function getUnionBounds(timeline:Timeline):BoundingRectangle {
+		var unionBounds:BoundingRectangle = { left: 0, top: 0, right: 0, bottom: 0 };
+		for (frame in 0...timeline.frameCount) {
+			var bounds:BoundingRectangle = timeline.getBounds(frame + 1 , false);
+			unionBounds.left   = Math.min(unionBounds.left, bounds.left);
+			unionBounds.top    = Math.min(unionBounds.top, bounds.top);
+			unionBounds.right  = Math.max(unionBounds.right, bounds.right);
+			unionBounds.bottom = Math.max(unionBounds.bottom, bounds.bottom);
+		}
+		return unionBounds;
 	}
 	
 }
